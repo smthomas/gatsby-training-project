dca83e03e6b3ef80498c6d98796f65f7001b87db
Shane Thomas <shane@thirdandgrove.com>
2019-11-22 02:09:59 -0500

    Completed Nested Layouts section

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,11 +8,20 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import { css } from "@emotion/core"
 
 import Header from "./header"
+import Footer from "./footer"
 import "./layout.css"
 
 const Layout = ({ children }) => {
+  const wrapperStyle = css`
+    margin: 0 auto;
+    max-width: 960px;
+    padding: 0px 1.0875rem 1.45rem;
+    padding-top: 0;
+  `;
+
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -26,20 +35,9 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
+      <div css={wrapperStyle}>
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+        <Footer />
       </div>
     </>
   )
