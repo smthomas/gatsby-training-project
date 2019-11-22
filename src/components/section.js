742c6e85a3b62489d6ee81d5fa40083fcf7e6876
Shane Thomas <shane@thirdandgrove.com>
2019-11-22 01:38:13 -0500

    Completed Gatsby Styles section

--- src/components/section.js
+++ src/components/section.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React from "react"
+import { css } from "@emotion/core"
 
-const Section = ({ title, text}) => (
-  <>
+const container = css`
+  margin: 3rem auto;
+  max-width: 600px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+const Section = ({ title, text }) => (
+  <div css={container}>
     <h2>{title}</h2>
     <p>{text}</p>
-  </>
-);
+  </div>
+)
 
-export default Section;
+export default Section
